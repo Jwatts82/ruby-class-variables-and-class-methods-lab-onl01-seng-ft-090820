037e4e8421006b0f52d5565fff4f0f02a8fc5298
Jessica Watts <jessperr182@hotmail.com>
2020-09-16 19:35:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,2 +1,15 @@
 class Song 
+  attr_accessor :name, :artist, :genre 
+  
+  @@count = 0 
+
+  def self.count 
+    @@count
+  end
+  
+  def initialize (name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1 
   
