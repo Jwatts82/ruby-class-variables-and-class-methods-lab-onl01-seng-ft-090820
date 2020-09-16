26f34f5410be93667aee3cc90e8026a36981f670
Jessica Watts <jessperr182@hotmail.com>
2020-09-16 19:45:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,10 +3,34 @@ class Song
   
   @@count = 0 
   
-  @@ genres = []
-  
   def self.count 
     @@count
+  end
+  
+  @@ genres = []
+  
+  def self.genre_count
+    genre_count = {}
+    @@genre_count.each do |genre|
+      if genre_count[ggenre]
+        genre_count[genre] = 1 
+      end
+    end
+    genre_count
+  end
+  
+  @@ artists = []
+  
+  def self.artist_count
+    artist_count = {}
+    @@artist_count.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else
+        artist_count[artist] = 1 
+      end
+    end
+    artist_count
   end
   
    def self.artist
@@ -16,7 +40,9 @@ class Song
   def initialize (name, artist, genre)
     @name = name
     @artist = artist
+    @@artists << artist
     @genre = genre
+    @@genres << genre
     @@count += 1 
     
   
